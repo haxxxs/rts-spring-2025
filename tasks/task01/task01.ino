ae72e3caf577f2f10c48a16f647e1cbfe751f305
GitHub User <user@example.com>
2025-04-05 20:59:48 +0400

    1-2 labs Rodionov

--- tasks/task01/task01.ino
+++ tasks/task01/task01.ino
@@ -1,7 +1,57 @@
-void setup() {
+unsigned long time;
+int ledPin = 9;      
+  
+int val = 0;     
 
-}
+int pin3flag = 0;
+int pin5flag = 0;
+int pin6flag = 0;
+int pin9flag = 0;
+int pin10flag = 0;
 
-void loop() {
+const unsigned long periods[] = {10000, 1000, 500, 100, 50};
 
-}
+unsigned long previousMillis3 = 0;        
+unsigned long previousMillis5 = 0;       
+unsigned long previousMillis6 = 0;      
+unsigned long previousMillis9 = 0;    
+unsigned long previousMillis10 = 0; 
+
+
+    void setup() {
+      pinMode(3, OUTPUT);
+      pinMode(5, OUTPUT);
+      pinMode(6, OUTPUT);
+      pinMode(9, OUTPUT); 
+      pinMode(10, OUTPUT); 
+    }
+
+    void loop() {
+
+      time = micros();
+
+        if (time - previousMillis3 >= periods[0]){
+        PORTD ^= B00001000;
+        previousMillis3 = time;
+        }
+
+        if (time - previousMillis5 >= periods[1]){
+        PORTD ^= B00100000;
+        previousMillis5 = time;
+        }
+
+        if (time - previousMillis6 >= periods[2]){
+        PORTD ^= B01000000;
+        previousMillis6 = time;
+        }
+
+        if (time - previousMillis9 >= periods[3]){
+        PORTB ^= B00000010;
+        previousMillis9 = time;
+        }
+        
+        if (time - previousMillis10 >= periods[4] ){
+        PORTB ^= B00000100;
+        previousMillis10 = time;
+        }
+    }
--- tasks/task02/task02.ino
+++ tasks/task02/task02.ino
@@ -1,7 +1,92 @@
-void setup() {
+#include <TaskManagerIO.h>
 
+const int NUMBER_OF_PINS = 5;
+
+unsigned long* prevMicrosArr = new unsigned long[NUMBER_OF_PINS];
+unsigned long* intervalsArr = new unsigned long[NUMBER_OF_PINS];
+int* ledStates = new int[NUMBER_OF_PINS];
+int pins[NUMBER_OF_PINS] = {3, 5, 6, 9, 10};
+
+void setup() {
+  for (int i = 0; i < NUMBER_OF_PINS; ++i) {
+    prevMicrosArr[i] = 0;
+    ledStates[i] = LOW;
+    pinMode(pins[i], OUTPUT);
+  }
+
+  intervalsArr[0] = 10000;
+  intervalsArr[1] = 1000;
+  intervalsArr[2] = 500;
+  intervalsArr[3] = 94;
+  intervalsArr[4] = 42;
+  
+  taskid_t taskId0 = taskManager.scheduleFixedRate(intervalsArr[0], [] {
+    if (ledStates[0] == LOW) {
+      ledStates[0] = HIGH;
+    }
+    else {
+      ledStates[0] = LOW;
+    }
+
+    digitalWrite(pins[0], ledStates[0]);
+  }, TIME_MICROS);
+
+  taskid_t taskId1 = taskManager.scheduleFixedRate(intervalsArr[1], [] {
+    if (ledStates[1] == LOW) {
+      ledStates[1] = HIGH;
+    }
+    else {
+      ledStates[1] = LOW;
+    }
+
+    digitalWrite(pins[1], ledStates[1]);
+  }, TIME_MICROS);
+
+  taskid_t taskId2 = taskManager.scheduleFixedRate(intervalsArr[2], [] {
+    if (ledStates[2] == LOW) {
+      ledStates[2] = HIGH;
+    }
+    else {
+      ledStates[2] = LOW;
+    }
+
+    digitalWrite(pins[2], ledStates[2]);
+  }, TIME_MICROS);
+
+  taskid_t taskId3 = taskManager.scheduleFixedRate(intervalsArr[3], [] {
+    if (ledStates[3] == LOW) {
+      ledStates[3] = HIGH;
+    }
+    else {
+      ledStates[3] = LOW;
+    }
+
+    digitalWrite(pins[3], ledStates[3]);
+  }, TIME_MICROS);
+
+  taskid_t taskId4 = taskManager.scheduleFixedRate(intervalsArr[4], [] {
+    if (ledStates[4] == LOW) {
+      ledStates[4] = HIGH;
+    }
+    else {
+      ledStates[4] = LOW;
+    }
+
+    digitalWrite(pins[4], ledStates[4]);
+  }, TIME_MICROS);
+
+  taskid_t taskId5 = taskManager.scheduleFixedRate(intervalsArr[5], [] {
+    if (ledStates[5] == LOW) {
+      ledStates[5] = HIGH;
+    }
+    else {
+      ledStates[5] = LOW;
+    }
+
+    digitalWrite(pins[5], ledStates[5]);
+  }, TIME_MICROS);
 }
 
 void loop() {
-
+  taskManager.runLoop();
 }
